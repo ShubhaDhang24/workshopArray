2ec0e399604278141f0d317b828064007eb9212e
Shubha Dhang <shubhadhang@gmail.com>
2023-08-02 09:45:54 +0200

    declaration and initialization of an array

--- src/main/java/org/example/ArrayFunction.java
+++ src/main/java/org/example/ArrayFunction.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class ArrayFunction {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //declaration and initialization of string
+        String a1[]= {"Erik Sve","Milia Peter","Shubha D"};
+        arrayPrint(a1);
+
     }
+        public static  void arrayPrint(String[] a1)
+        {
+            for(int i=0;i< a1.length;i++)
+            {
+                System.out.println(a1[i]);
+            }
+
+        }
+
 }
